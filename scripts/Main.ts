c007c0cc8bdaaf7859101cded1c247871b817678
SvenFrankson <sven.frankson@gmail.com>
2017-12-02 14:12:47 +0100

    BabylonJS empty project setup.

---
+++ scripts/Main.ts
@@ -0,0 +1,42 @@
+class Main {
+
+	public canvas: HTMLCanvasElement;
+	public engine: BABYLON.Engine;
+	public scene: BABYLON.Scene;
+	public light: BABYLON.HemisphericLight;
+	public camera: BABYLON.ArcRotateCamera;
+
+	constructor(canvasElement: string) {
+		this.canvas = document.getElementById(canvasElement) as HTMLCanvasElement;
+		this.engine = new BABYLON.Engine(this.canvas, true);
+	}
+	
+	createScene(): void {
+		this.scene = new BABYLON.Scene(this.engine);
+		this.resize();
+
+		this.light = new BABYLON.HemisphericLight("Light", (new BABYLON.Vector3(0.5, 0.65, 0.8)).normalize(), this.scene);
+		this.light.groundColor.copyFromFloats(0.5, 0.5, 0.5);
+		this.light.intensity = 0.7;
+
+		this.camera = new BABYLON.ArcRotateCamera("MenuCamera", Math.PI/3, Math.PI / 3, 10, BABYLON.Vector3.Zero(), this.scene);
+		this.camera.attachControl(this.canvas);
+	}
+
+	public animate(): void {
+		this.engine.runRenderLoop(() => {
+			this.scene.render();
+		});
+	}
+
+	public resize(): void {
+		this.engine.resize();
+	}
+}
+
+
+window.addEventListener("DOMContentLoaded", () => {
+	let game: Main = new Main("render-canvas");
+	game.createScene();
+	game.animate();
+});
---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+class Main {
+    constructor(canvasElement) {
+        this.canvas = document.getElementById(canvasElement);
+        this.engine = new BABYLON.Engine(this.canvas, true);
+    }
+    createScene() {
+        this.scene = new BABYLON.Scene(this.engine);
+        this.resize();
+        this.light = new BABYLON.HemisphericLight("Light", (new BABYLON.Vector3(0.5, 0.65, 0.8)).normalize(), this.scene);
+        this.light.groundColor.copyFromFloats(0.5, 0.5, 0.5);
+        this.light.intensity = 0.7;
+        this.camera = new BABYLON.ArcRotateCamera("MenuCamera", Math.PI / 3, Math.PI / 3, 10, BABYLON.Vector3.Zero(), this.scene);
+        this.camera.attachControl(this.canvas);
+    }
+    animate() {
+        this.engine.runRenderLoop(() => {
+            this.scene.render();
+        });
+    }
+    resize() {
+        this.engine.resize();
+    }
+}
+window.addEventListener("DOMContentLoaded", () => {
+    let game = new Main("render-canvas");
+    game.createScene();
+    game.animate();
+});
